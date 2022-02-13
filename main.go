efbc60e7c8b0bd1573ed6f9743f4db0bd4deee8a
frankfang <frankfang1990@gmail.com>
2022-02-13 22:00:19 +0800

    init

--- main.go
+++ main.go
@@ -3,19 +3,25 @@ package main
 import (
 	"os"
 	"os/signal"
+	"path/filepath"
 	"syscall"
 
 	"github.com/zserge/lorca"
 )
 
 func main() {
-	var ui lorca.UI
-	ui, _ = lorca.New("https://baidu.com", "", 800, 600, "--disable-sync", "--disable-translate")
+	chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+	// start chrome
+
+	// var ui lorca.UI
+	// currentDir, _ := os.Getwd()
+	// dir := filepath.Join(currentDir, ".cache")
+	// ui, _ = lorca.New("https://baidu.com", dir, 800, 600)
 	chSignal := make(chan os.Signal, 1)
 	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
 	select {
-	case <-ui.Done():
+	// case <-ui.Done():
 	case <-chSignal:
 	}
-	ui.Close()
+	// ui.Close()
 }
